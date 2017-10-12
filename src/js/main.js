89109bc3b60361d9c1901a235ec8045389531028
Duarte Carrilho da Graça <duarte.carrilho.casual@theguardian.com>
2017-10-12 16:48:47 +0100

    work in progress for chris

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,9 @@
 var el = document.createElement('script');
 el.src = '<%= path %>/app.js';
 document.body.appendChild(el);
+
+
+var iframeBody = document.querySelector('body');
+var parentBody = parent.document.querySelector('body');
+
+iframeBody.setAttribute('class', parentBody.getAttribute('class'));
